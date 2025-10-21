bfe1572c70eda37f40718901f1396b2a6186df25
wikswroom <alexander.wikstrom@pinewik.se>
2025-10-21 23:34:55 +0200

    rubrik

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -38,7 +38,7 @@ export default function Features() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-gray-600">
-            Allt du behöver
+            Bara det du behöver
           </h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Ett enklare sätt att hantera företaget
